1d4dea6e89361948e6f6a3dd976f530ca51eaf0c
unknown <sivaranjanib99@gmail.com>
2021-07-11 23:36:46 +0530

    [siva]UC1 finding the frequency of the word in hash table

---
+++ HashTable/Hash.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+  
+        class Hash<K, V>
+        {
+            private readonly int listSize;
+            private LinkedList<KeyValue<K, V>>[] item;
+            public Hash(int size)
+            {
+                this.listSize = size;
+                this.item = new LinkedList<KeyValue<K, V>>[size];
+            }
+            public int GetArrayPosition(K key)
+            {
+                int position = key.GetHashCode() % listSize;
+                return (Math.Abs(position));
+
+            }
+            protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+            {
+                LinkedList<KeyValue<K, V>> linkedList = item[position];
+                if (linkedList == null)
+                {
+                    linkedList = new LinkedList<KeyValue<K, V>>();
+                    item[position] = linkedList;
+                }
+                return linkedList;
+            }
+            public void AddNode(K key, V value)
+            {
+                int position = GetArrayPosition(key);
+                LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+                KeyValue<K, V> keyValue = new KeyValue<K, V> { Key = key, Value = value };
+                linkedList.AddLast(keyValue);
+            }
+            public void display()
+            {
+                for (int i = 0; i < listSize; i++)
+                {
+                    LinkedList<KeyValue<K, V>> linkedList = item[i];
+                    if (linkedList == null)
+                    {
+                        continue;
+                    }
+                    foreach (KeyValue<K, V> list in linkedList)
+                    {
+                        Console.WriteLine("key:{0},value:{1}", list.Key, list.Value);
+                    }
+                }
+            }
+            public int frequency(K key)
+            {
+                int count = 0;
+                int position = GetArrayPosition(key);
+                LinkedList<KeyValue<K, V>> linkedList = item[position];
+                if (linkedList == null)
+                {
+                    return count;
+                }
+                else
+                {
+                    foreach (KeyValue<K, V> list in linkedList)
+                    {
+                        if (list.Key.Equals(key) == true)
+                        {
+                            count++;
+                        }
+                    }
+                    return count;
+                }
+            }
+        }
+
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+
+        }
+}
+
+
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,6 +7,15 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hashtable!!");
+            Hash<string, int> hash = new Hash<string, int>(6);
+            hash.AddNode("To", 1);
+            hash.AddNode("be", 2);
+            hash.AddNode("or", 3);
+            hash.AddNode("not",4);
+            hash.AddNode("To", 1);
+            hash.AddNode("be", 2);
+            hash.display();
+            Console.WriteLine("The frequency of To is {0}", hash.frequency("to"));
         }
     }
 }
